57f28a8f0d621ec3ecbfc1d274fa9b18b259e35c
Jeter807 <156726547+Jeter807@users.noreply.github.com>
2024-02-18 20:26:24 -0500

    Add files via upload

---
+++ task5/numberAverage.js
@@ -0,0 +1,11 @@
+// numberAverage.js
+const NumberAverage = {
+    Avg: function(num1, num2) {
+        return (num1 + num2) / 2.0;
+    },
+    AvgOfThree: function(num1, num2, num3) {
+        return (num1 + num2 + num3) / 3.0;
+    }
+};
+
+module.exports = NumberAverage;
---
+++ task5/test.js
@@ -0,0 +1,8 @@
+// test.js
+const NumberAverage = require('./numberAverage');
+
+// Test the Avg function
+console.log('Average of 10 and 20:', NumberAverage.Avg(10, 20));
+
+// Test the AvgOfThree function
+console.log('Average of 10, 20, and 30:', NumberAverage.AvgOfThree(10, 20, 30));
